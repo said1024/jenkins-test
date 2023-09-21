7feff71dd1c7555248b6e9d225bacfe5883fb565
said <said1024.b@gmail.com>
2023-09-21 10:24:23 +0100

    first commit

---
+++ src/main/java/com/said/jenkins_test/JenkinsTestApplication.java
@@ -0,0 +1,15 @@
+package com.said.jenkins_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestApplication.class, args);
+		System.out.println("bonjour");
+
+	}
+	
+}
---
+++ src/test/java/com/said/jenkins_test/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.said.jenkins_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
